d9d480462386648ee8a4e629eb3386bc056090a5
Philippe Ozil <pozil@salesforce.com>
2025-02-13 09:19:04 +0100

    feat: reset script pushes Apex/LWC

--- demo-reset.sh
+++ demo-reset.sh
@@ -3,6 +3,7 @@ SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $SCRIPT_PATH
 
 git reset --hard HEAD
+sf project deploy start -c -g -m ApexClass -m LightningComponentBundle
 
 echo ""
 echo Demo was reset. Remember to close all VS Code tabs.
