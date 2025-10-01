29f544ce8831da1c08077e1a61a435ac6c8669a0
Philippe Ozil <pozil@salesforce.com>
2025-10-01 16:52:09 +0200

    feat: updated reset script

--- reset-demo.sh
+++ reset-demo.sh
@@ -3,8 +3,8 @@ SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $SCRIPT_PATH
 
 git reset --hard HEAD
-sf project deploy start -c -g -m ApexClass -m LightningComponentBundle
+sf project deploy start -c -g
 
 echo ""
-echo Demo was reset. Remember to close all VS Code tabs.
+echo Demo was reset. Remember to close all IDE tabs and clear dev agent history.
 echo ""
